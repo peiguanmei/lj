fe6089164da720bd4de7fb98c477daa501aac5cf
peiguanmei <peiguanmei@163.com>
2019-09-23 12:40:34 +0800

    update

---
+++ leetcode/src/test/java/com/p/arrays/easy/ArrayPartition1/Solution561Test.java
@@ -0,0 +1,16 @@
+package com.p.arrays.easy.ArrayPartition1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution561Test {
+
+    @Test
+    public void arrayPairSum() {
+        Solution561 solution = new Solution561();
+        int[] nums = {1, 3, 4, 2, 9, 15};
+        System.out.println(solution.arrayPairSum(nums));
+    }
+
+}
